f5a1dd998d65b385cd477f62190f7596b384d093
Mlody87 <kabatpawel@gmail.com>
2020-05-11 18:47:44 +0200

    Parowanie kolorami jednej grupy
    Teraz kolej na weryfikacje czy juz grali ze soba

---
+++ Serwer/swiss.py
@@ -0,0 +1,141 @@
+from enum import IntEnum
+import collections
+
+class Colour(IntEnum):
+    black = -1
+    none  =  0,
+    white =  1
+
+
+class swissPairing:
+    def __init__(self, round, players, last_round=False):
+        self.round = round
+        self.players = players
+        self.last_round = last_round
+        self.pairing()
+
+
+
+    def checkIsBye(self):
+        if (self.isOdd(len(self.players))):
+            self.isBye = 1
+        else:
+            self.isBye = 0
+
+    def sort(self, by):
+        self.players = sorted(self.players, key=lambda i: i[by], reverse=True)
+
+    def setPairingNo(self):
+        self.sort('rating')
+        i=1
+        for k in self.players:
+            k['pairing_no'] = i
+            k['score'] = 0
+            k['color_hist'] = ()
+            k['opponents'] = ()
+            i+=1
+
+    def isOdd(self, num):
+        if (num % 2) == 0:
+            return False
+        else:
+            return True
+
+    def setBye(self):
+        for player in reversed(self.players):
+            if(0 not in player['opponents']):
+                player['score'] += 1
+                player['color_hist'] += (0,)
+                player['opponents'] += (0,)
+                break
+
+    def getPlayerByPN(self, pn):
+        for k in self.players:
+            if(k['pairing_no'] == pn):
+                return k
+
+    def pair1round(self):
+        if(self.isBye>0):
+            self.setBye()
+
+        half = len(self.players)//2
+        for i in range(1, half+1):
+            op1 = self.getPlayerByPN(i)
+            op2 = self.getPlayerByPN(half+i)
+
+            if(not self.isOdd(i)):
+                op1['opponents']=(op2['pairing_no'],)
+                op1['color_hist'] += (-1,)
+
+                op2['opponents']=(op1['pairing_no'],)
+                op2['color_hist'] += (1,)
+            else:
+                op1['opponents']=(op2['pairing_no'],)
+                op1['color_hist'] += (1,)
+
+                op2['opponents']=(op1['pairing_no'],)
+                op2['color_hist'] += (-1,)
+
+
+    def createGroups(self):
+        result = collections.defaultdict(list)
+
+        for d in self.players:
+            result[d['score']].append(d)
+
+        result_list = list(result.values())
+        return result_list
+
+    def removeByePlayer(self, groups):
+        for i in range(len(groups)):
+            for j in range(len(groups[i])):
+                if (len(groups[i][j]['opponents']) == self.round):
+                    del groups[i][j]
+                    break
+        return groups
+
+    def pairRound(self):
+        self.sort('score')
+        if (self.isBye > 0):
+            self.setBye()
+
+        groups = self.createGroups()
+        groups = self.removeByePlayer(groups)
+
+
+
+
+
+
+    def pairing(self):
+        self.checkIsBye()
+
+        if(self.round==1):
+            self.setPairingNo()
+            self.pair1round()
+            print(self.players)
+        else:
+            self.pairRound()
+
+
+
+
+runda1 = (
+            {'name':'Bruno', 'rating':2500},
+            {'name':'Alice', 'rating':2400},
+            {'name':'Carla', 'rating':2600},
+            {'name':'Eloise', 'rating':2350},
+            {'name':'Giorgia', 'rating':2555},
+)
+
+swissPairing(1, runda1)
+
+runda2 = (
+     {'name': 'Carla', 'rating': 2600, 'pairing_no': 1, 'score': 1, 'color_hist': (1,), 'opponents': (3,)},
+     {'name': 'Giorgia', 'rating': 2555, 'pairing_no': 2, 'score': 1, 'color_hist': (-1,), 'opponents': (4,)},
+     {'name': 'Bruno', 'rating': 2500, 'pairing_no': 3, 'score': 0, 'color_hist': (-1,), 'opponents': (1,)},
+     {'name': 'Alice', 'rating': 2400, 'pairing_no': 4, 'score': 0, 'color_hist': (1,), 'opponents': (2,)},
+     {'name': 'Eloise', 'rating': 2350, 'pairing_no': 5, 'score': 1, 'color_hist': (0,), 'opponents': (0,)}
+)
+
+swissPairing(2, runda2)
--- Serwer/test.py
+++ Serwer/test.py
@@ -1,115 +1,212 @@
-from enum import IntEnum
+import math
 
-class Colour(IntEnum):
-    black = -1
-    none  =  0,
-    white =  1
+def isOdd(num):
+    if (num % 2) == 0:
+        return False
+    else:
+        return True
+
+def getPlayerByPN(group, pn):
+    for k in group:
+        if(k['pairing_no'] == pn):
+            return k
+
+def pairGroup(no):
+    group = lista[no]
+
+    if (no >= center):
+        dir = +1
+    else:
+        dir = -1
+
+    if((len(group)<2)):
+        for i in group:
+            lista[no+dir].append(group[i])
+
+        if(no!=center):
+            pairGroup(no+dir)
+
+    prefW = {}
+    prefB = {}
+    prefN = {}
+    drop = set()
+
+    for k in group:
+        suma = sum(k['color_hist'])
+        if(suma>0):
+            prefB[k['pairing_no']] = suma
+        if(suma<0):
+            prefW[k['pairing_no']] = suma
+        if(suma==0):
+            prefN[k['pairing_no']] = suma
+
+    #sorted(prefW, key=lambda i: i['pairing_no'], reverse=True)
+    #sorted(prefB, key=lambda i: i['pairing_no'], reverse=True)
+    #sorted(prefN, key=lambda i: i['pairing_no'], reverse=True)
+
+    print(prefW)
+    print(prefB)
+    print(prefN)
+
+    print("----------------")
 
 
-class swissPairing:
-    def __init__(self, round, players, last_round=False):
-        self.round = round
-        self.players = players
-        self.last_round = last_round
-        self.pairing()
 
-    def checkIsBye(self):
-        if (self.isOdd(len(self.players))):
-            self.isBye = 1
+    whiteToDel = set()
+    tmpPairs = list()
+
+    for k in prefW:
+        if(len(prefB)>0):
+            pair = (k, next(iter(prefB.keys())))
+            tmpPairs.append(pair)
+            del prefB[next(iter(prefB.keys()))]
+            whiteToDel.add(k)
         else:
-            self.isBye = 0
-
-    def sort(self, by):
-        self.players = sorted(self.players, key=lambda i: i[by], reverse=True)
-
-    def setPairingNo(self):
-        self.sort('rating')
-        i=1
-        for k in self.players:
-            k['pairing_no'] = i
-            k['score'] = 0
-            k['color_hist'] = ()
-            k['opponents'] = ()
-            i+=1
-
-    def isOdd(self, num):
-        if (num % 2) == 0:
-            return False
-        else:
-            return True
-
-    def setBye(self):
-        for player in reversed(self.players):
-            if(0 not in player['opponents']):
-                player['score'] += 1
-                player['color_hist']=(0,)
-                player['opponents']=(0,)
+            if(len(prefN)>0):
+                pair = (k, next(iter(prefN.keys())))
+                tmpPairs.append(pair)
+                del prefN[next(iter(prefN.keys()))]
+                whiteToDel.add(k)
+            else:
                 break
 
-    def getPlayerByPN(self, pn):
-        for k in self.players:
-            if(k['pairing_no'] == pn):
-                return k
+    for k in whiteToDel:
+        del prefW[k]
 
-    def pair1round(self):
-        if(self.isBye>0):
-            self.setBye()
+    print(prefW)
+    print(prefB)
+    print(prefN)
 
-        half = len(self.players)//2
-        for i in range(1, half+1):
-            op1 = self.getPlayerByPN(i)
-            op2 = self.getPlayerByPN(half+i)
 
-            if(not self.isOdd(i)):
-                op1['opponents']=(op2['pairing_no'],)
-                op1['color_hist']=(-1,)
+    if(len(prefW)>0):
+        print("Czarne i None wykorzystane. Zostaly same biale. Parujemy tylko biale")
 
-                op2['opponents']=(op1['pairing_no'],)
-                op2['color_hist']=(1,)
+        if (len(prefW) % 2 != 0):
+            drop.add(list(prefW)[-1],)
+            del prefW[list(prefW)[-1]]
+
+        for v, w in zip(list(prefW)[::2], list(prefW)[1::2]):
+            pair = (v, w)
+            tmpPairs.append(pair)
+            del prefW[v]
+            del prefW[w]
+
+
+    else:
+        print("Wszystkie biale wykorzystane. Sprawdzam czy sa czarne")
+
+        blackToDel = set()
+        for k in prefB:
+            if (len(prefN) > 0):
+                pair = (k, next(iter(prefN.keys())))
+                tmpPairs.append(pair)
+                del prefN[next(iter(prefN.keys()))]
+                blackToDel.add(k)
             else:
-                op1['opponents']=(op2['pairing_no'],)
-                op1['color_hist']=(1,)
+                break
 
-                op2['opponents']=(op1['pairing_no'],)
-                op2['color_hist']=(-1,)
+        for k in blackToDel:
+            del prefB[k]
 
+        print(prefW)
+        print(prefB)
+        print(prefN)
 
-    def pairRound(self):
-        self.sort('score')
-        if (self.isBye > 0):
-            self.setBye()
+        if (len(prefB) > 0):
+            print("Nie ma juz bialych. Wszystkie None wykorzystane. Zostaly jeszcze czarne. Parujemy tylko czarne")
 
-        
+            if (len(prefB) % 2 != 0):
+                drop.add(list(prefB)[-1],)
+                del prefB[list(prefB)[-1]]
 
-    def pairing(self):
-        self.checkIsBye()
+            for v, w in zip(list(prefB)[::2], list(prefB)[1::2]):
+                pair = (v, w)
+                tmpPairs.append(pair)
+                del prefB[v]
+                del prefB[w]
 
-        if(self.round==1):
-            self.setPairingNo()
-            self.pair1round()
-            print(self.players)
         else:
-            self.pairRound()
+            if(len(prefN)>0):
+                print("Nie ma juz bialy i czarnych. Zostaly same Noney. Paruje Nony")
+
+                if (len(prefN) % 2 != 0):
+                    drop.add(list(prefN)[-1],)
+                    del prefN[list(prefN)[-1]]
+
+                for v, w in zip(list(prefN)[::2], list(prefN)[1::2]):
+                    pair = (v, w)
+                    tmpPairs.append(pair)
+                    del prefN[v]
+                    del prefN[w]
+
+
+    print("Pierwotne pary")
+    print(tmpPairs)
+    print(prefW)
+    print(prefB)
+    print(prefN)
+    print(drop)
+
+    print("------------")
+    print("Weryfikacja parowań")
+
+    pairs = set()
+    for index, item in enumerate(tmpPairs):
+        print("Sprawdzam pare", item)
+
+        op = getPlayerByPN(group, item[1])
+
+        if(item[0] in op['opponents']):
+            print("Juz grali")
 
 
 
 
-runda1 = (
-            {'name':'Bruno', 'rating':2500},
-            {'name':'Alice', 'rating':2400},
-            {'name':'Carla', 'rating':2600},
-            {'name':'Eloise', 'rating':2350},
-            {'name':'Giorgia', 'rating':2555}
-)
+    print("----------")
+    print("Ostateczne pary")
+    print(pairs)
 
-swissPairing(1, runda1)
 
-runda2 = (
-     {'name': 'Carla', 'rating': 2600, 'pairing_no': 1, 'score': 1, 'color_hist': (1,), 'opponents': (3,)},
-     {'name': 'Giorgia', 'rating': 2555, 'pairing_no': 2, 'score': 1, 'color_hist': (-1,), 'opponents': (4,)},
-     {'name': 'Bruno', 'rating': 2500, 'pairing_no': 3, 'score': 0, 'color_hist': (-1,), 'opponents': (1,)},
-     {'name': 'Alice', 'rating': 2400, 'pairing_no': 4, 'score': 0, 'color_hist': (1,), 'opponents': (2,)},
-     {'name': 'Eloise', 'rating': 2350, 'pairing_no': 5, 'score': 1, 'color_hist': (0,), 'opponents': (0,)}
-)
 
-swissPairing(2, runda2)
+
+lista = [
+    [{'name': 'Carla', 'rating': 2600, 'pairing_no': 1, 'score': 1, 'color_hist': (-1,), 'opponents': (11,)},
+{'name': 'Giorgia', 'rating': 2555, 'pairing_no': 11, 'score': 1, 'color_hist': (1,1), 'opponents': (1,)},
+          {'name': 'Giorgia', 'rating': 2555, 'pairing_no': 2, 'score': 1, 'color_hist': (-1,), 'opponents': (4,)},
+{'name': 'Giorgia', 'rating': 2555, 'pairing_no': 20, 'score': 1, 'color_hist': (1,), 'opponents': (4,)},
+{'name': 'Giorgia', 'rating': 2555, 'pairing_no': 30, 'score': 1, 'color_hist': (0,), 'opponents': (4,)},
+{'name': 'Giorgia', 'rating': 2555, 'pairing_no': 31, 'score': 1, 'color_hist': (0,), 'opponents': (4,)},
+{'name': 'Giorgia', 'rating': 2555, 'pairing_no': 32, 'score': 1, 'color_hist': (0,), 'opponents': (4,)},
+
+
+
+
+          {'name': 'Alice', 'rating': 2400, 'pairing_no': 4, 'score': 1, 'color_hist': (1, 0), 'opponents': (2, 0)}],
+
+         [{'name': 'Bruno', 'rating': 2500, 'pairing_no': 3, 'score': 0, 'color_hist': (-1,), 'opponents': (1,)}]
+]
+
+runda = 2
+
+for i in range(len(lista)):
+    for j in range(len(lista[i])):
+        if(len(lista[i][j]['opponents'])==runda):
+            del lista[i][j]
+            break
+
+
+groupsNo = len(lista)
+center = math.ceil((groupsNo/2))
+
+pairGroup(0)
+
+
+
+
+
+
+#1. dzielimy na pol i zaokraglamy do gory OK
+#2. sprawdzamy 1 grupe OK
+#3. jezeli liczba w grupie <2 zrzucamy wszystkich w dol jezeli > polowa, w gore<polowa OK
+#4. parujemy, jezeli nieparzysta, zrzucamy ostatniego, ktory w dol jezeli > polowa, w gore<polowa
+#5. jezeli ktos nie moze miec pary rzucamy  w dol jezeli > polowa, w gore<polowa
