c24d2cea08b4ce939a6e76d80671741280ee939c
Mlody87 <kabatpawel@gmail.com>
2020-05-14 00:44:48 +0200

    Podzial na rowne grupy
    Problem z typami list

--- Swiss/swiss.py
+++ Swiss/swiss.py
@@ -8,10 +8,12 @@ class SwissEngine:
     def __init__(self, round, players):
         self._round = round
         self._players = list(players)
-        self._center = None
-        self._sortedGroups = None
         self._byePlayer = None
         self._checkPlayersAgain = False
+        self._groupsNo = None
+        self._g1 = list()
+        self._g2 = list()
+        self._g0 = list()
 
         if(self._round==1):
             self._firstRound()
@@ -59,21 +61,25 @@ class SwissEngine:
 
         groups = list(result.values())
 
-        groupsNo = len(groups)
-        self._center = math.ceil(groupsNo / 2) if not groupsNo % 2 else math.ceil((groupsNo / 2)) - 1
+        self._groupsNo = len(groups)
 
-        g1 = groups[:self._center]
-        g2 = groups[self._center:]
-        finalGroups = list()
 
-        while g1:
-            p1 = g1.pop(0)
-            finalGroups.append(p1)
-        while g2:
-            p2 = g2.pop()
-            finalGroups.append(p2)
+        top = True
+        for i in range(0, (self._groupsNo)):
+            if (len(groups) == 1):
+                self._g0.append(groups.pop(0))
+                break
 
-        return finalGroups
+            if (top):
+                self._g1.append(groups.pop(0))
+            else:
+                self._g2.append(groups.pop(-1))
+
+            if (top):
+                top = False
+            else:
+                top = True
+
 
     def _floatDirection(self, i):
         if i> self._center:
@@ -88,52 +94,122 @@ class SwissEngine:
         if dir<0:
             return 1
 
-    def _preparePlayers(self, groupNo):
-        group = self._sortedGroups[groupNo]
-        dir = self._floatDirection(groupNo)
+    def _preparePlayers(self, dir, groupNo):
+        center=False
+        if(dir>0):
+            source = self._g1
+        if(dir<0):
+            source = self._g2
+        if(dir==0):
+            source = self._g0
+            center=True
+
+        group = source[groupNo]
+
+        if(len(list(group))==0):
+            return
 
         group.sort(key=lambda Player: Player.score,
                            reverse=True)
 
-        if(groupNo != self._center):
+        if(not center):
+
             if (len(group) % 2):
                 drop = group.pop(-1)
-                self._sortedGroups[groupNo+dir].append(drop)
+                if((len(source)-1)==groupNo):
+                    self._g0.append(drop)
+                else:
+                    source[groupNo+1].append(drop)
 
             for p in group:
                 opponents = copy.deepcopy(group)
                 opponents.remove(p)
                 if(not p.can_play(opponents)):
                     drop = group.pop(-1)
-                    self._sortedGroups[groupNo + dir].append(drop)
+                    if ((len(source) - 1) == groupNo):
+                        self._g0.append(drop)
+                    else:
+                        source[groupNo + 1].append(drop)
 
-            if(len(group)<2):
-                self._sortedGroups[groupNo + dir] + group
+            if (len(group) == 1):
+                if ((len(source) - 1) == groupNo):
+                    self._g0 += group
+                else:
+                    source[groupNo + 1] += group
                 return
+
         else:
-            if(len(group)<2):
-                dir = self._flipDir(dir)
-                self._sortedGroups[groupNo + dir] + group
+            if(len(group)==1):
+                if(self._groupsNo % 2 == 0):
+                    dropTo = self._g1[-1]
+                else:
+                    dropTo = self._g2[-1]
+
+                dropTo += group
                 self._checkPlayersAgain = True
                 return
+
             canPlay = True
             for p in group:
                 if(not p.can_play(group)):
                     canPlay = False
             if(not canPlay):
-                dir = self._flipDir(dir)
-                self._sortedGroups[groupNo + dir] + group
+                if (self._groupsNo % 2 == 0):
+                    dropTo = self._g1[-1]
+                else:
+                    dropTo = self._g2[-1]
+
+                dropTo += group
                 self._checkPlayersAgain = True
+
+    def _createGroupsAgaing(self):
+        groups = list()
+        groups += self._g1
+        groups += self._g2
+        groups += self._g0
+
+        self._groupsNo = len(groups)
+
+        groupsC = copy.deepcopy(groups)
+
+        top = True
+        for i in range(0, (len(groups))):
+            if (len(groupsC) == 1):
+                self._g0.append(groupsC.pop(0),)
+                break
+
+            if (top):
+                self._g1.append(groupsC.pop(0),)
+            else:
+                self._g2.append(groupsC.pop(-1),)
+
+            if (top):
+                top = False
+            else:
+                top = True
+
+        return True
+
 
     def _pairingGroups(self):
 
         while True:
-            for index, item in enumerate(self._sortedGroups):
-                self._preparePlayers(index)
-            if(self._checkPlayersAgain==False):
-                break
+            #Pair top groups
+            for index, item in enumerate(self._g1):
+                self._preparePlayers(1,index)
 
-        print(self._sortedGroups)
+            #Pair bottom groups
+            for index, item in enumerate(self._g2):
+                self._preparePlayers(-1,index)
+
+            #Pair center group
+            for index, item in enumerate(self._g0):
+                self._preparePlayers(0,index)
+
+            if(self._checkPlayersAgain):
+                self._createGroupsAgaing()
+            else:
+                break
 
     def _checkBye(self):
         if(len(self._players) % 2):
@@ -146,7 +222,7 @@ class SwissEngine:
         self._players.sort(key=lambda Player: Player.score,
                            reverse=True)
         self._checkBye()
-        self._sortedGroups = self._createGroups()
+        self._createGroups()
 
         firstpairing = self._pairingGroups()
 
--- Swiss/testy.py
+++ Swiss/testy.py
@@ -1,56 +1,24 @@
-from swiss import SwissEngine
-from player import Player
-import math
-
-input_players = (
-        Player(name='Alice',
-               score=1,
-               pairing_no=1,
-               opponents=(2,),
-               colour_hist=(1,),
-               rating=2300),
-        Player(name='Pablo',
-                score=0,
-pairing_no=2,
-               opponents=(1,),
-               colour_hist=(-1,),
-               rating=2200),
-        Player(name='Qba',
-               rating=2100,
-pairing_no=3,
-                score=1,
-               opponents=(4,),
-               colour_hist=(1,)),
-        Player(name='Tester',
-               rating=2000,
-pairing_no=4,
-                score=0,
-               opponents=(3,),
-               colour_hist=(-1,)),
-                Player(name='ktos',
-               rating=1900,
-pairing_no=5,
-                score=1,
-                opponents=(0,),
-                colour_hist=(0,),
-                       ),
-Player(name='Draw1',
-               rating=2155,
-pairing_no=6,
-                score=0.5,
-                opponents=(7,),
-                colour_hist=(1,),
-                       ),
-Player(name='Draw2',
-               rating=2166,
-pairing_no=7,
-                score=0.5,
-                opponents=(6,),
-                colour_hist=(-1,),
-                       )
-)
-
-engine = SwissEngine(2, input_players)
+class A:
+    def __init__(self,nr):
+        self._nr = nr
 
 
+lista = []
 
+a = A(1)
+b = A(1)
+c = A(1)
+d = A(1)
+e = A(1)
+f = A(1)
+
+podlistaA = [a,b,c]
+podlistaB = [d,e]
+podlistaC = [f,]
+
+lista.append(podlistaA)
+lista.append(podlistaB)
+lista.append(podlistaC)
+
+for x in lista:
+    print("Rozmiar: ",len(x))
